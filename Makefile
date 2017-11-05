1102af4305069b0d2913c5fd618cb70172d51f5d
Mike Barker <mike@thebarkers.com>
2017-11-05 11:03:59 -0700

    Lesson 3, Sections 9-12

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
 # variables
 SERVER_NAME=activities-server
 
+DB_CONTAINER_NAME=activities-database
+DB_DATA_DIR=$(PWD)/Data
+DB_SEED_DIR=$(PWD)/Seed
+DB_DATABASE=game_night
+DB_USER=root
+DB_PASSWORD=password
+DB_HOST=172.17.0.2
+DB_PORT=3306
+DB_IMAGE=mysql-database
+
 # targets
 hello_s3: hello_swift hello_kitura
 	@echo "what are you building?"
@@ -12,11 +22,35 @@ hello_swift:
 hello_kitura:
 	@echo "hello kitura"
 
-web_dev: build_web_image
+web_run_bash: web_build
 	docker run --name $(SERVER_NAME) \
-	-it --rm -v $(PWD):/src \
-	-w /src \
-	-p 80:8080 kitura-server /bin/bash
+		-it --rm -v $(PWD):/src \
+		-w /src \
+		-p 80:8080 kitura-server /bin/bash
 
-build_web_image:
+web_build:
 	docker build -t kitura-server -f Dockerfile-web .
+
+db_run_seed: db_stop db_clean
+	docker run \
+		-d \
+		--name $(DB_CONTAINER_NAME) \
+		-e MYSQL_ROOT_PASSWORD=$(DB_PASSWORD) \
+		-e MYSQL_DATABASE=$(DB_DATABASE) \
+		--expose $(DB_PORT) \
+		-p $(DB_PORT):$(DB_PORT) \
+		-v $(DB_SEED_DIR):/docker-entrypoint-initdb.d \
+		-v $(DB_DATA_DIR):/var/lib/mysql \
+		$(DB_IMAGE) \
+		--character-set-server=utf8mb4 --collation-server=utf8mb4_bin
+
+db_stop:
+	@docker stop $(DB_CONTAINER_NAME) || true && docker rm $(DB_CONTAINER_NAME) || true
+
+db_clean:
+	rm -rf $(DB_DATA_DIR)
+	mkdir -p $(DB_DATA_DIR)
+
+db_connect_shell:
+	docker run --name mysql-shell -it \
+		--rm mysql sh -c 'exec mysql -h$(DB_HOST) -P$(DB_PORT) -u$(DB_USER) -p$(DB_PASSWORD) --default-character-set=utf8mb4'
--- Sources/main.swift
+++ Sources/main.swift
@@ -24,19 +24,50 @@ var pool = MySQLConnectionPool(connectionString: connectionString, poolSize: 10,
 // Get a connection, insert a dummy activity
 do {
     try pool.getConnection() { (connection: MySQLConnectionProtocol) in
-        let result = try connection.execute(query: "INSERT INTO activities (name, genre, description, emoji, min_participants, max_participants) VALUES ('New Activity', 'Puzzle', 'A simple dummy game.', '🎲', '2', '4');")
-        if result.affectedRows > 0 {
-            Log.info("activity inserted")
-        } else {
-            Log.error("activity not inserted")
-        }
+      // show all activities
+      let selectQuery = "SELECT * FROM activities;"
+      let selectResult = try connection.execute(query: selectQuery)
+      while case let row? = selectResult.nextResult() {
+        print(row)
+      }
+
+      // try to insert an activity
+      let insertQuery = "INSERT INTO activities " + 
+        "(name, genre, description, emoji, min_participants, max_participants) " + 
+        "VALUES ('New Activity', 'Puzzle', 'A simple dummy game.', '\u{1F3B2}', '2', '4');"
+      let insertResult = try connection.execute(query: insertQuery)
+      if insertResult.affectedRows > 0 {
+        print("activity was inserted")
+      } else {
+        print("activity not inserted")
+      }
+
+      // try to update an activity
+      let updateQuery = "UPDATE activities SET name='Yahtzee', genre='Chance', " +
+        "description='Roll the dice!', emoji='\u{1F3B2}', min_participants='2', " +
+        "max_participants='9' WHERE id=7;"
+      let updateResult = try connection.execute(query: updateQuery)
+      if updateResult.affectedRows > 0 {
+        print("activity was updated")
+      } else {
+        print("activity not updated")
+      }
+
+      // try to delete an activity
+      let deleteQuery = "DELETE FROM activities WHERE id=7"
+      let deleteResult = try connection.execute(query: deleteQuery)
+      if deleteResult.affectedRows > 0 {
+        print("activity was deleted")
+      } else {
+        print("activity not deleted")
+      }
     }
 } catch {
     Log.error(error.localizedDescription)
 }
 
 // Add an HTTP server and connect it to the router
-Kitura.addHTTPServer(onPort: 8080, with: Router())
+// Kitura.addHTTPServer(onPort: 8080, with: Router())
 
 // Start the Kitura runloop (this call never returns)
-Kitura.run()
+// Kitura.run()
